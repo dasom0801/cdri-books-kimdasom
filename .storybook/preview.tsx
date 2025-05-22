079f55c16b22a0b306856162f1cf745eaef34e9b
Dasom <dev.dasom1012@gmail.com>
2025-05-22 20:44:17 +0900

    chore: storybook 설치 및 설정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router';
+const queryClient = new QueryClient();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      </QueryClientProvider>
+    ),
+  ],
+};
+
+export default preview;
