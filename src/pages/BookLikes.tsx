054ccb8e25d592e8352cbb9bd68cd25c76e43ba7
Dasom <dev.dasom1012@gmail.com>
2025-05-23 20:54:29 +0900

    feat: route 설정

---
+++ src/pages/BookLikes.tsx
@@ -0,0 +1,3 @@
+export default function BookLikes() {
+  return <div>BookLikes</div>;
+}
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export { default as BookLikes } from './BookLikes';
+export { default as BookSearch } from './BookSearch';
